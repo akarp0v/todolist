f45d6a4570ba5b16e0ba6508671209d4f89e2803
akarp0v <komhleb@yandex.ru>
2021-03-13 23:35:53 +0300

    text editor added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,27 +10,43 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-let inc = 0;
-
 document.body.querySelector('.save_button').addEventListener('click', (event) => {
   let textInput = document.body.querySelector('.text_input');
   let divTasks = document.body.querySelector('.task_list');
 
   const divElem = document.createElement('div');
   divElem.className = 'task';
-  divElem.dataset.id = inc;
   divElem.innerHTML = `
-        <div class='task_title'>${textInput.value}</div>
-        <button class='delete_button' value=``>x</button>`;
+    <div class='task_title'>${textInput.value}</div>
+    <button class='delete_button'>x</button>
+  `;
   divTasks.appendChild(divElem);
 
-  inc++;
   textInput.value = '';
 });
 
-document.body.querySelector('.task_list').addEventListener('click', (event) => {
+document.body.querySelector('.task_list').addEventListener('click', function (event) {
   let target = event.target;
   if (target.className.includes('delete_button')) {
     target.parentElement.remove();
+  }
+});
+
+document.body.querySelector('.task_list').addEventListener('dblclick', function (event) {
+  let target = event.target;
+  
+  if (target.className.includes('task_title')) {
+    target.innerHTML = `<textarea class="edit_text" id="newcont" rows="1">${target.textContent}</textarea>`;
+    target.focus();
+  }
+});
+
+document.body.querySelector('.task_list').addEventListener('click', function (event) {
+  let target = event.target;
+  let textAreaElem = document.body.querySelector('#newcont');
+
+  if (!(target.className.includes('edit_text')) && textAreaElem !== null) {
+    textAreaElem.parentElement.textContent = textAreaElem.value;
+    textAreaElem.remove();
   }
 });
